765d2d9cedf1b8fbbb5f7958bc27220898fabb20
Yashraj0405 <yashrajpatil.co.in@gmail.com>
2026-04-22 09:48:02 +0530

    Configuration for setting up h2database

--- patient-service/src/main/java/com/pm/patientService/model/Patient.java
+++ patient-service/src/main/java/com/pm/patientService/model/Patient.java
@@ -1,17 +1,37 @@
 package com.pm.patientService.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
+@Data
 @Entity
 public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private  String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private  LocalDate registeredDate;
+
 }
